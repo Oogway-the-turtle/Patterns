3741b1999b6265c8d21371cca28266a9f397ed62
Eugene <puskasevgenij@gmail.com>
2024-09-03 17:15:52 +0700

    Initial commit

---
+++ src/test/java/data/DataGenerator.java
@@ -0,0 +1,49 @@
+package data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        String[] city = new String[]{"Новосибирск", "Барнаул", "Омск", "Томск", "Екатеринбург", "Томск",
+                "Челябинск", "Сыктывкар", "Махачкала", "Грозный", "Брянск", "Вологда",};
+        return city[new Random().nextInt(city.length)];
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
